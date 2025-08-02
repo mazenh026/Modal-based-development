1549d1a97f6e5a9de6df96b24be0fd1fb8067cfd
mazenh026 <mazenh026@gmail.com>
2025-08-03 01:04:09 +0300

    Add files via upload

---
+++ soil_moisture_lab/untitled_ert_rtw/untitled.c
@@ -0,0 +1,36 @@
+/*
+ * File: untitled.c
+ *
+ * Code generated for Simulink model 'untitled'.
+ *
+ * Model version                  : 1.0
+ * Simulink Coder version         : 9.9 (R2023a) 19-Nov-2022
+ * C/C++ source code generated on : Sat Jul  5 19:13:47 2025
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives:
+ *    1. Execution efficiency
+ *    2. RAM efficiency
+ * Validation result: Not run
+ */
+
+#include "untitled.h"
+
+/* Model step function */
+void untitled_step(void)
+{
+  /* (no output/update code required) */
+}
+
+/* Model initialize function */
+void untitled_initialize(void)
+{
+  /* (no initialization code required) */
+}
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
